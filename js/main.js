a4c421df5b7d739745a57338a48cd7286fbd2e84
Alexandre Shiotuki <106483068+AlexandreShiotuki@users.noreply.github.com>
2022-08-20 16:42:22 -0300

    NOVAS MUNDANÇAS - A HISTÓRIA DA ARTE
    Adicionados:
    - JavaScript
    - Novo tema - a arte nos jogos
    - Fontes disponíveis no final da página

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function load() {
+  alert("Bem-vindo, este site foi feito apenas para fins didáticos, todos os textos presentes foram retirados de vários sites, nos quais deixarei a fonte no final da página, nenhum texto ou imagem é de minha propriedade!");
+}
+
+
+function WhatIsJourney() {
+  window.open("https://osurtododia.com.br/journey-o-jogo-que-e-uma-obra-de-arte/");
+}
+
+function WhatIsLis() {
+  window.open("https://www.terra.com.br/gameon/entenda-a-historia-dos-jogos-da-serie-life-is-strange,2fd354c93b4e0808f1098740e33659b5n42fvw8f.html");
+}
